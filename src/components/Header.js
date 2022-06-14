7c41ef18343a173da24b264b0146149d35d2b39b
deepakn9944 <deepakn9944@gmail.com>
2022-06-14 16:40:38 +0530

    cryptoFinder

---
+++ src/components/Header.js
@@ -0,0 +1,70 @@
+import { AppBar, Container, createTheme, Toolbar, Typography, makeStyles, ThemeProvider, MenuItem, Select } from '@material-ui/core';
+
+import React from 'react'
+import {  useNavigate } from "react-router-dom";
+import { CryptoState } from '../CryptoContext';
+import AuthModal from './Authentication/AuthModal';
+import UserSidebar from './Authentication/UserSidebar';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#fff",
+    },
+    type: "dark",
+  },
+});
+
+
+
+const Header = () => {
+
+   const classes = useStyles();
+
+  const navigate = useNavigate();
+
+  const { currency, setCurrency, user} = CryptoState();
+
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => navigate("/")}
+              variant="h6"
+              className={classes.title}
+            >
+              Crypto Finder
+            </Typography>
+            <Select
+              variant="outlined"
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={currency}
+              style={{ width: 100, height: 40, marginLeft: 15 }}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+            {user ? <UserSidebar /> : <AuthModal />}
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+}
+
+export default Header;
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBK5erOMd2W1uUMhmRH76vlho3sEA4oXO8",
+  authDomain: "crypto-finder-1f96f.firebaseapp.com",
+  projectId: "crypto-finder-1f96f",
+  storageBucket: "crypto-finder-1f96f.appspot.com",
+  messagingSenderId: "413561366005",
+  appId: "1:413561366005:web:a9e78976f57084bfc2acbc",
+};
+
+export default firebaseConfig;
